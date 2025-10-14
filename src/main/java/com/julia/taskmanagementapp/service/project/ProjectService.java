c44ce618c66125688b5b5b711403545374b6f6f4
Yuliia <innishastudy@gmail.com>
2025-10-14 17:40:39 +0300

    removed unused import

--- src/main/java/com/julia/taskmanagementapp/service/project/ProjectService.java
+++ src/main/java/com/julia/taskmanagementapp/service/project/ProjectService.java
@@ -3,10 +3,8 @@ package com.julia.taskmanagementapp.service.project;
 import com.julia.taskmanagementapp.dto.project.CreateProjectRequestDto;
 import com.julia.taskmanagementapp.dto.project.ProjectDto;
 import com.julia.taskmanagementapp.dto.project.UpdateProjectRequestDto;
-import com.julia.taskmanagementapp.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 public interface ProjectService {
     ProjectDto create(CreateProjectRequestDto requestDto);
