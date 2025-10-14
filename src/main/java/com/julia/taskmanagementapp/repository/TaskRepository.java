2b2bc1441c9b1ad578466315ba9ccf705a900e77
Yuliia Tatarchuk <153382678+YuliiaNisha@users.noreply.github.com>
2025-10-14 18:16:24 +0300

    Implement get user projects in project controller (#9)
    * implemented getUserProjects method in projectController
    
    * removed unused import

--- src/main/java/com/julia/taskmanagementapp/repository/TaskRepository.java
+++ src/main/java/com/julia/taskmanagementapp/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.julia.taskmanagementapp.repository;
 
 import com.julia.taskmanagementapp.model.Task;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findByProjectId(Long projectId, Pageable pageable);
+
+    List<Task> findByAssigneeId(Long userId);
 }
--- src/main/java/com/julia/taskmanagementapp/service/project/ProjectService.java
+++ src/main/java/com/julia/taskmanagementapp/service/project/ProjectService.java
@@ -3,9 +3,13 @@ package com.julia.taskmanagementapp.service.project;
 import com.julia.taskmanagementapp.dto.project.CreateProjectRequestDto;
 import com.julia.taskmanagementapp.dto.project.ProjectDto;
 import com.julia.taskmanagementapp.dto.project.UpdateProjectRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProjectService {
     ProjectDto create(CreateProjectRequestDto requestDto);
+
+    Page<ProjectDto> getUserProjects(Long userId, Pageable pageable);
 
     ProjectDto getProjectById(Long id);
 
