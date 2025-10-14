3c49530159bb9001602cc07ab4480aa8a09a7de2
Yuliia <innishastudy@gmail.com>
2025-10-14 16:16:16 +0300

    added task id check and improved delete method

--- src/main/java/com/julia/taskmanagementapp/service/task/TaskServiceImpl.java
+++ src/main/java/com/julia/taskmanagementapp/service/task/TaskServiceImpl.java
@@ -8,6 +8,8 @@ import com.julia.taskmanagementapp.mapper.TaskMapper;
 import com.julia.taskmanagementapp.model.Task;
 import com.julia.taskmanagementapp.repository.ProjectRepository;
 import com.julia.taskmanagementapp.repository.TaskRepository;
+import com.julia.taskmanagementapp.repository.UserRepository;
+import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,9 +21,20 @@ public class TaskServiceImpl implements TaskService {
     private final TaskMapper taskMapper;
     private final TaskRepository taskRepository;
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
 
     @Override
     public TaskDto create(CreateTaskRequestDto requestDto) {
+        entityExistsById(
+                requestDto.projectId(),
+                projectRepository::existsById,
+                "project"
+        );
+        entityExistsById(
+                requestDto.assigneeId(),
+                userRepository::existsById,
+                "user"
+        );
         Task task = taskMapper.toModel(requestDto);
         task.setStatus(Task.Status.NOT_STARTED);
         Task savedTask = taskRepository.save(task);
@@ -30,39 +43,49 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Page<TaskDto> getTasksForProject(Long projectId, Pageable pageable) {
-        projectExists(projectId);
+        entityExistsById(
+                projectId,
+                projectRepository::existsById,
+                "project"
+        );
         return taskRepository.findByProjectId(projectId, pageable)
                 .map(taskMapper::toDto);
     }
 
     @Override
     public TaskDto getTaskById(Long id) {
-        Task task = taskRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("There is no task by id: " + id)
-        );
+        Task task = findTaskById(id);
         return taskMapper.toDto(task);
     }
 
     @Override
     public TaskDto update(Long id, UpdateTaskRequestDto requestDto) {
-        Task task = taskRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("There is no task by id: " + id)
-        );
+        Task task = findTaskById(id);
         taskMapper.updateTask(task, requestDto);
         return taskMapper.toDto(taskRepository.save(task));
     }
 
     @Override
     public void delete(Long id) {
-        taskRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("There is no task by id: " + id)
-        );
-        taskRepository.deleteById(id);
+        Task task = findTaskById(id);
+        taskRepository.delete(task);
     }
 
-    private void projectExists(Long projectId) {
-        if (!projectRepository.existsById(projectId)) {
-            throw new EntityNotFoundException("There is no project by id: " + projectId);
+    private void entityExistsById(
+            Long id,
+            Predicate<Long> existsPredicate,
+            String entityName
+    ) {
+        if (!existsPredicate.test(id)) {
+            throw new EntityNotFoundException(
+                    "There is no " + entityName + " by id: " + id
+            );
         }
+    }
+
+    private Task findTaskById(Long id) {
+        return taskRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("There is no task by id: " + id)
+        );
     }
 }
