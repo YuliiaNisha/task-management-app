233ed72b7124be4a1762b5e720d0cc646e0bb16a
Yuliia <innishastudy@gmail.com>
2025-10-15 19:46:50 +0300

    implemented getLabels and update methods

--- src/main/java/com/julia/taskmanagementapp/dto/label/UpdateLabelRequestDto.java
+++ src/main/java/com/julia/taskmanagementapp/dto/label/UpdateLabelRequestDto.java
@@ -1,13 +1,10 @@
 package com.julia.taskmanagementapp.dto.label;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CreateLabelRequestDto (
-        @NotBlank(message = "Label name is required")
+public record UpdateLabelRequestDto(
         @Size(max = 50, message = "Label name must be less than 50 characters")
         String name,
-        @NotBlank(message = "Label color is required")
         @Size(max = 25, message = "Label color name must be less than 25 characters")
         String color
 ) {
--- src/main/java/com/julia/taskmanagementapp/service/label/LabelServiceImpl.java
+++ src/main/java/com/julia/taskmanagementapp/service/label/LabelServiceImpl.java
@@ -2,11 +2,16 @@ package com.julia.taskmanagementapp.service.label;
 
 import com.julia.taskmanagementapp.dto.label.CreateLabelRequestDto;
 import com.julia.taskmanagementapp.dto.label.LabelDto;
+import com.julia.taskmanagementapp.dto.label.UpdateLabelRequestDto;
 import com.julia.taskmanagementapp.exception.EntityAlreadyExistsException;
+import com.julia.taskmanagementapp.exception.EntityNotFoundException;
 import com.julia.taskmanagementapp.mapper.LabelMapper;
 import com.julia.taskmanagementapp.model.Label;
 import com.julia.taskmanagementapp.repository.LabelRepository;
+import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -17,27 +22,91 @@ public class LabelServiceImpl implements LabelService {
 
     @Override
     public LabelDto create(CreateLabelRequestDto requestDto) {
-        labelNameExists(requestDto.name().trim());
-        labelColorExists(requestDto.color().trim());
-        Label label = labelMapper.toModel(requestDto);
+        CreateLabelRequestDto trimmedRequestDto = labelMapper.trimCreateRequest(requestDto);
+        checkValuesAlreadyExist(trimmedRequestDto.name(), trimmedRequestDto.color());
+        Label label = labelMapper.toModel(trimmedRequestDto);
         return labelMapper.toDto(labelRepository.save(label));
     }
 
-    private void labelColorExists(String color) {
-        if (labelRepository.existsByColorIgnoreCase(color)) {
-            throw new EntityAlreadyExistsException(
-                    "Cannot create label with color '" + color
-                            + "' - This color is already in use. Please choose another color."
+    @Override
+    public Page<LabelDto> getLabels(Pageable pageable) {
+        return labelRepository.findAll(pageable)
+                .map(labelMapper::toDto);
+    }
+
+    @Override
+    public LabelDto update(Long id, UpdateLabelRequestDto requestDto) {
+        Label label = findLabelById(id);
+        UpdateLabelRequestDto trimmedRequestDto = labelMapper.trimUpdateRequest(requestDto);
+        checkValuesAlreadyExistOrDuplicate(
+                trimmedRequestDto.name(),
+                trimmedRequestDto.color(),
+                label.getName(),
+                label.getColor()
+        );
+        labelMapper.update(label, trimmedRequestDto);
+        return labelMapper.toDto(labelRepository.save(label));
+    }
+
+    private void checkValuesAlreadyExistOrDuplicate(
+            String name, String color,
+            String currentName, String currentColor
+    ) {
+        if (name != null && !name.equalsIgnoreCase(currentName)) {
+            checkField(
+                    name,
+                    labelRepository::existsByNameIgnoreCase,
+                    new EntityAlreadyExistsException(
+                            "Cannot update label with name '" + name
+                                    + "' because it already exists. Please choose a different name."
+                    )
+            );
+        }
+        if (color != null && !color.equalsIgnoreCase(currentColor)) {
+            checkField(
+                    color,
+                    labelRepository::existsByColorIgnoreCase,
+                    new EntityAlreadyExistsException(
+                            "Cannot update label with color '" + color
+                                    + "' - This color is already in use. Please choose another color."
+                    )
             );
         }
     }
 
-    private void labelNameExists(String name) {
-        if (labelRepository.existsByNameIgnoreCase(name)) {
-            throw new EntityAlreadyExistsException(
-                    "Label with name '" + name
-                            + "' already exists. Please choose a different name."
+    private void checkValuesAlreadyExist(String name, String color) {
+        if (name != null) {
+            checkField(
+                    name,
+                    labelRepository::existsByNameIgnoreCase,
+                    new EntityAlreadyExistsException("Label with name '" + name
+                            + "' already exists. Please choose a different name.")
             );
         }
+        if (color != null) {
+            checkField(
+                    color,
+                    labelRepository::existsByColorIgnoreCase,
+                    new EntityAlreadyExistsException("Cannot create label with color '" + color
+                            + "' - This color is already in use. Please choose another color.")
+            );
+        }
+    }
+
+    private void checkField(
+            String fieldValue,
+            Predicate<String> predicate,
+            RuntimeException exception) {
+        if (predicate.test(fieldValue)) {
+            throw exception;
+        }
+    }
+
+    private Label findLabelById(Long id) {
+        return labelRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(
+                        "There is no label by id: " + id
+                )
+        );
     }
 }
